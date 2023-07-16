91f9a32097c4a2871e51f9be9e5d33a835b66012
TailonNecke <tailon.necke@gmail.com>
2023-07-16 14:19:36 -0300

    base site

---
+++ Pages/Home/script.js
@@ -0,0 +1,8 @@
+function OpenNav(){
+        document.getElementById("sidebar").style.left = "0px";
+        document.getElementById("controllCloseSideBar").style.display = "block";
+}
+function CloseNav(){
+    document.getElementById("sidebar").style.left = "-250px";
+    document.getElementById("controllCloseSideBar").style.display = "none";
+}
